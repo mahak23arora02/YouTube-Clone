afa61c4f53673670436ef6b9e0de7bf170537bc0
Mahak Arora <mahak23arora02@gmail.com>
2020-07-02 16:14:40 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import VideoDetail from './VideoDetail';
+
+export { default as SearchBar } from './SearchBar';
+export { default as VideoDetail } from './VideoDetail';
+export { default as VideoList } from './VideoList';
